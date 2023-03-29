e8f70e677a6d982f439d18199e49459b458f5da4
Lorenzo Cherubin <lorenzo.cherubin@gmail.com>
2023-03-29 13:42:40 +0200

    Most of the homepage done

---
+++ components/exploreButton.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+
+export const ExploreButton = ({ onClick }: { onClick?: () => void }) => (
+  <Link
+    className={
+      'group relative flex h-36 w-36 cursor-pointer items-center justify-center rounded-full bg-tertiary  md:h-[15rem] md:w-[15rem] lg:h-[17.125rem] lg:w-[17.125rem]'
+    }
+    href={'/destinations'}
+  >
+    <div
+      className={
+        'absolute -inset-0 -z-10 h-full w-full transform rounded-full  bg-tertiary/10 transition-all duration-500 ease-in-out group-hover:scale-[160%]  '
+      }
+    ></div>
+    <h3 className={'font-serif text-lg text-primary  lg:text-[2rem]/[2.25rem]'}>
+      EXPLORE
+    </h3>
+  </Link>
+)
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,8 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import Layout from '@/components/Layout'
 import MobileBackground from '@/public/assets/home/background-home-mobile.jpg'
 import TabletBackground from '@/public/assets/home/background-home-tablet.jpg'
 import DesktopBackground from '@/public/assets/home/background-home-desktop.jpg'
-
-const inter = Inter({ subsets: ['latin'] })
+import { ExploreButton } from '@/components/exploreButton'
 
 export default function Home() {
   return (
@@ -18,42 +15,38 @@ export default function Home() {
       >
         <div
           className={
-            'flex flex-col items-center justify-center gap-4 pt-4 text-center'
-          }
-        >
-          <h2 className={'font-condensed tracking-subtitle text-secondary'}>
-            SO, YOU WANT TO TRAVEL TO
-          </h2>
-          <h1 className="font-serif text-[5rem] leading-[6.25rem] text-tertiary">
-            SPACE
-          </h1>
-          <p className={'font-sans text-[0.9375rem] leading-6 text-secondary'}>
-            Let’s face it; if you want to go to space, you might as well
-            genuinely go to outer space and not hover kind of on the edge of it.
-            Well sit back, and relax because we’ll give you a truly out of this
-            world experience!
-          </p>
-        </div>
-        {/*[28.125rem]*/}
-        <div
-          className={
-            'group flex h-48 w-48  items-center  justify-center rounded-full md:h-[22rem] md:w-[22rem]  lg:h-[28.125rem] lg:w-[28.125rem]'
+            'flex h-full flex-col items-center justify-between pb-6 md:justify-around'
           }
         >
           <div
             className={
-              'flex h-36 w-36 cursor-pointer items-center justify-center rounded-full bg-tertiary bg-opacity-10 transition-all duration-500 ease-in-out hover:h-48 hover:w-48 md:h-[15rem] md:w-[15rem] md:hover:h-[22rem] md:hover:w-[22rem] lg:h-[17.125rem] lg:w-[17.125rem] lg:hover:h-[28.125rem] lg:hover:w-[28.125rem] '
+              ' flex max-w-xs flex-col items-center justify-center gap-4 pt-4 text-center md:max-w-md'
             }
           >
-            <div
+            <h2
               className={
-                'flex h-36 w-36 items-center justify-center rounded-full bg-tertiary md:h-[15rem] md:w-[15rem] lg:h-[17.125rem] lg:w-[17.125rem]'
+                'font-condensed tracking-subtitle text-secondary md:text-xl/[1.5rem]'
               }
             >
-              <h3 className={'font-serif text-lg text-primary lg:text-[2rem]'}>
-                EXPLORE
-              </h3>
-            </div>
+              SO, YOU WANT TO TRAVEL TO
+            </h2>
+            <h1 className="font-serif text-[5rem] leading-[6.25rem] text-tertiary md:text-[9.375rem]/[9.375rem] ">
+              SPACE
+            </h1>
+            <p
+              className={
+                ' font-sans text-[0.9375rem] leading-6 text-secondary md:px-2 md:text-base/[1.75rem] '
+              }
+            >
+              Let’s face it; if you want to go to space, you might as well
+              genuinely go to outer space and not hover kind of on the edge of
+              it. Well sit back, and relax because we’ll give you a truly out of
+              this world experience!
+            </p>
+          </div>
+          {/*[28.125rem]*/}
+          <div>
+            <ExploreButton onClick={() => console.log('button')} />
           </div>
         </div>
       </Layout>
