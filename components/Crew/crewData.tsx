0d38608f40fa75c9e5665c262cb3740f7073a11b
Lorenzo Cherubin <53137532+lolelu@users.noreply.github.com>
2023-04-02 19:53:51 +0200

    Changese to credata and crewimages

--- components/Crew/crewData.tsx
+++ components/Crew/crewData.tsx
@@ -2,8 +2,13 @@ import { CrewMember } from '@/types/data'
 
 const CrewData = ({ crewMember, isActive }: { crewMember: CrewMember; isActive: boolean }) => {
   return (
-    <div className={`${isActive ? 'visible' : 'invisible'} col-start-1 row-start-3   `}>
-      <h4>{crewMember.name}</h4>
+    <div
+      className={`${
+        isActive ? 'visible' : 'invisible'
+      } col-start-1 row-start-3 flex flex-col items-center justify-center text-center     `}>
+      <p className={'font-serif text-base uppercase text-tertiary text-opacity-50'}>{crewMember.role}</p>
+      <h4 className={'font-serif text-2xl/[1.75rem] uppercase text-tertiary'}>{crewMember.name}</h4>
+      <p className={'text-[.9375rem]/[1.5625rem] text-secondary'}>{crewMember.bio}</p>
     </div>
   )
 }
--- components/Crew/crewImage.tsx
+++ components/Crew/crewImage.tsx
@@ -3,8 +3,11 @@ import { CrewMember } from '@/types/data'
 
 const CrewImage = ({ crewMember, isActive }: { crewMember: CrewMember; isActive: boolean }) => {
   return (
-    <div className={`${isActive ? 'visible' : 'invisible'} col-start-1 row-start-1 `}>
+    <div className={`${isActive ? 'visible' : 'invisible'} col-start-1 row-start-1  justify-self-center`}>
       <Image src={crewMember.images.webp} alt={crewMember.name} width={300} height={300} className={'w-[11rem]'} />
+
+      {/*  Da muovere fuori dalla componenente*/}
+      <hr className={' w-full border-t-2 border-tertiary border-opacity-50'} />
     </div>
   )
 }
